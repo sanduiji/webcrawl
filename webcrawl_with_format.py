0ebc4d3a6e275d01aef877d034519015031f8c74
sanduiji <54425603+sanduiji@users.noreply.github.com>
2022-10-17 10:34:51 +0800

    Update webcrawl_with_format.py

--- webcrawl_with_format.py
+++ webcrawl_with_format.py
@@ -6,7 +6,7 @@ import html2text
 import html2markdown
 
 
-# 修改浏览器信息。python自带浏览器会被反爬虫
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
 }
